d74440f52a8272410d23128a2ae7769841ec1ee5
MattScieszinski <mattski01@icloud.com>
2020-08-09 22:27:33 -0500

    renamed app.js from script.js

---
+++ js/app.js
@@ -0,0 +1,54 @@
+// Ask username
+
+var  userName = prompt('Enter name, please.');
+
+if (userName == "Matt") {
+    alert('Howdy, Matt!');
+    var greeting1 = alert('Have a look, ' +  userName + '!')
+}
+
+var  Order = confirm ('Press OK if you need some food recommendations');
+var greeting1 = alert('Have a look, ' +  userName + '!');
+
+
+
+
+
+ 
+// ask where they are
+
+
+
+// ask if they're hungry
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if(hourNow > 18) {
+    greeting = "It's Time for Dinner, " + userName + '!';
+} else if (hourNow > 12) {
+    greeting = "Have a Great Luchtime, " + userName + '!';
+} else if (hourNow > 0) {
+    greeting = "Breakfast is Ready, " + userName + '!';
+} else {
+    greeting = "Restaurants aren't open at this time, " + userName;
+}
+
+Get.textContent()
+
+
+// Salutations
+
+
+
+
+
+
+
+//21+ special greeting
+
+
+
+
+
+
